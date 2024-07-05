13c304453447144e8346acb64834143a5c61fc6a
gil <ryogo5.0304@gmail.com>
2024-07-05 12:33:05 +0000

    db & user system

--- app.rb
+++ app.rb
@@ -6,3 +6,31 @@ require './models.rb'
 get '/' do
   erb :index
 end
+
+get '/signin' do
+  erb :sign_in
+end
+
+post '/signin' do
+  user = User.find_by(mail: params[:mail])
+  if user && user.authenticate(params[:password])
+    session[:user] = user.id
+    redirect '/'
+  else
+    redirect '/signin'
+  end
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signup' do
+  user = User.create(name: params[:name], mail: params[:mail], password: params[:password], password_confirmation: params[:password_confirmation])
+  if user.persisted?
+    session[:user] = user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+end
--- models.rb
+++ models.rb
@@ -2,3 +2,32 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :contents
+  has_many :comments
+  has_many :contents, through: :comments
+  
+  validates :name, presence: true, uniqueness: true
+  
+  validates :mail, presence: true, uniqueness: true
+
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).+\z/ }
+end
+
+class Content < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
+  has_many :users, through: :comments
+  
+  validates :title, presence: true
+  validates :body, presence: true
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :content
+  
+  validates :body, presence: true
+end
